2f1ad5ac52cb82e491962b9b5a91cc6f224d74b4
bklukaczewski <bklukaczewski@gmail.com>
2019-05-05 21:02:55 +0200

    Add more functions to TypeScript definition.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,16 @@
 declare module 'react-native-cookies' {
   export default class CookieManager {
-    clearAll(): Promise<void>;
+    static set(cookie: any): Promise<any>;
+
+    static setFromResponse(name: string, response: string): Promise<any>;
+
+    static get(name: string): Promise<any>;
+
+    static getAll(): Promise<any>;
+
+    static clearByName(name: string): Promise<any>;
+
+    static clearAll(): Promise<any>;
+
   }
 }

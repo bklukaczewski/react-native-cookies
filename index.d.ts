6d1b420318433e1ee88c84c8a01312844d7ddad1
bklukaczewski <bklukaczewski@gmail.com>
2019-05-06 13:49:00 +0200

    Support WebKit in TypeScript definition.

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,16 @@
 declare module 'react-native-cookies' {
   export default class CookieManager {
-    static set(cookie: any): Promise<any>;
+    static set(cookie: any, webKitSupport?: boolean): Promise<any>;
 
     static setFromResponse(name: string, response: string): Promise<any>;
 
-    static get(name: string): Promise<any>;
+    static get(name: string, webKitSupport?: boolean): Promise<any>;
 
-    static getAll(): Promise<any>;
+    static getAll(webKitSupport?: boolean): Promise<any>;
 
     static clearByName(name: string): Promise<any>;
 
-    static clearAll(): Promise<any>;
+    static clearAll(webKitSupport?: boolean): Promise<any>;
 
   }
 }
